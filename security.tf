cdf58a7d2b8be09eb50575953370fa2251d37b44
Marcos F. Villa <mvicha@gmail.com>
2023-11-28 16:18:58 +0100

    Change from gitlab.mvilla.org

--- security.tf
+++ security.tf
@@ -1,5 +1,5 @@
 module "policies" {
-  source = "git::https://gitlab.mvilla.org/mvicha/tempfinnet?ref=policies"
+  source = "git::https://github.com/mvicha/finnet.git?ref=policies"
 
   buckets       = var.buckets
   cloudfront_id = aws_cloudfront_distribution.mod_cloudfront_distribution.id
